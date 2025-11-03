7e7d88388dd0a254a7ce0b59d23e484895e3ae11
1xposed <karstenesserr@gmail.com>
2025-11-02 23:04:13 -0500

    fixes

---
+++ hangman.js
@@ -0,0 +1,77 @@
+var POSSIBLE_WORDS = ["obdurate", "verisimilitude", 
+    "defenestrate", "obsequious", "dissonant", "toady", "idempotent"];
+var MAX_GUESSES = 6;
+var word = "";
+var guesses = "";
+var guess_count = MAX_GUESSES;
+var lost = false;
+
+function newGame() {
+    var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
+    word = POSSIBLE_WORDS[randomIndex];
+    guesses = "";
+    guess_count = MAX_GUESSES;
+    lost = false;
+    updatePage();
+}
+
+function guessLetter() {
+    var input = document.getElementById("guess");
+    var letter = input.value;
+    input.value = "";
+    
+
+        if (guesses.indexOf(letter) >= 0){
+            document.getElementById("guesses").innerHTML = "letter already guessed";
+            return;
+        }
+
+//wrong
+    if(word.indexOf(letter) < 0){
+        guess_count--;
+    }
+
+
+    guesses += letter;
+    updatePage();
+}
+
+
+function updatePage() {
+    var clueString = "";
+   
+    for (var i = 0; i < word.length; i++) {
+        var currentLetter = word.charAt(i);
+        if (guesses.indexOf(currentLetter) >= 0) { // You guessed it!
+            clueString += currentLetter + " ";
+           
+        } 
+        else {
+            clueString += "_ ";
+            
+        }
+    }
+
+// update the clue string
+var clue = document.getElementById("clue");
+clue.innerHTML = clueString;
+
+// update the guesses from the user
+var guessArea = document.getElementById("guesses");
+guessArea.innerHTML = "Guessed Letters: " + guesses;
+
+// update the image
+var image = document.getElementById("hangmanImage");
+image.src = "images/hangman"+ guess_count + ".gif";
+    
+    document.getElementById("clue").innerHTML = clueString;
+
+
+if (guess_count === 0){
+
+     document.getElementById("guesses").innerHTML = "YOU LOSE";
+
+
+}
+
+}
